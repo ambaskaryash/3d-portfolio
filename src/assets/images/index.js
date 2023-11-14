cea02fad02b8346a885726558577cd3177778ef3
ambaskaryash <kimetsu119@gmail.com>
2023-11-14 17:26:09 +0530

    logo updated

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,7 +2,7 @@ import hero from './hero.jpg'
 import sitel from './sitel.png'
 import coreco from './coreco.png'
 import wipro from './wipro.png'
-import logo from './logo.svg'
+import logo from './logo1.svg'
 import graduate from './graduate.png'
 
 export {
